5e9cf55167724e884c1054f4af9d836e32a4fa05
Gehendrajungraya <127176235+Gehendrajungraya@users.noreply.github.com>
2026-08-18 19:52:02 +0545

    Delete lib directory

--- lib/config.dart
+++
@@ -1,8 +0,0 @@
-class AppConfig {
-  // Change this to your WordPress website URL.
-  static const String baseUrl = 'https://gehendrajung.com.np';
-  static const String apiBase = '$baseUrl/wp-json/gjps/v1';
-
-  // Optional: change these to match the API authentication implementation.
-  static const String appName = 'GJ Store';
-}
