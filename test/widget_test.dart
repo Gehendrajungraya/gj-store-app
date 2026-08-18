4178fa6b42b8d0a865b585165ac3fcaed4d59b2c
Gehendrajungraya <127176235+Gehendrajungraya@users.noreply.github.com>
2026-08-18 19:51:40 +0545

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:gj_store_app/main.dart';
-
-void main() {
-  testWidgets('GJ Store starts', (tester) async {
-    await tester.pumpWidget(const GJStoreApp());
-    expect(find.text('GJ STORE'), findsOneWidget);
-  });
-}
